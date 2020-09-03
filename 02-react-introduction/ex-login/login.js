ad3d648ef6faadc63f7ffdf22e4a72d3f60bb8dc
ywarezk <ywarezk@gmail.com>
2020-09-03 14:44:33 +0300

    finished the ex

--- 02-react-introduction/ex-login/login.js
+++ 02-react-introduction/ex-login/login.js
@@ -4,4 +4,68 @@
  * in react class turns to className
  */
  
+function Header() {
+	return (
+		<header>
+			<nav>
+				<h1>
+					this is the header
+				</h1>
+			</nav>
+		</header>
+	)
+}
+
+function Login() {
+	return (
+		<form>
+			<div className="form-group">
+				<label htmlFor="id" >Email</label>
+				<input id="email" type="email" className="form-control" />
+			</div>
+			<div className="form-group">
+				<label>Password</label>
+				<input type="password" className="form-control" />
+			</div>
+			<div className="form-group">
+				<button type="submit" className="btn btn-primary">
+					Login
+				</button>
+			</div>
+		</form>
+	)
+}
  
+function Footer() {
+	return (
+		<footer>
+			<h1>
+				I'm the footer
+			</h1>
+		</footer>
+	)
+}
+ 
+function App() {
+	return (
+		<div className="container-fluid h-100">
+			<div className="row h-100">
+				<div className="col-12 d-flex flex-column justify-content-between">
+				
+					<Header />
+					
+					<div className="d-flex justify-content-center">
+						<div className="col-6">
+							<Login />
+						</div>
+					</div>
+					
+					<Footer />
+				
+				</div>
+			</div>
+		</div>
+	)
+}
+
+ReactDOM.render(<App />, document.getElementById('root'));
---
+++ 05-components/README.md
@@ -0,0 +1,12 @@
+# React Components
+
+React essence is creating a UI components.
+In this lessons we will focus on the tools that we have with React for building the components and updating the components.
+
+## Lesson Plan
+
+- component can be a function or a class
+- props
+- state, useState
+- Events
+- useEffect
